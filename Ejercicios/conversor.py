1918bc4769ae712780705b86dd4876973f8c69f7
Natalia Garcia <ygaarcia@omd.com.co>
2023-02-16 11:46:37 -0500

    Se le agrego el if name

--- Ejercicios/conversor.py
+++ Ejercicios/conversor.py
@@ -7,22 +7,25 @@ def conversor(tipo_pesos, valor_dolar):
     print("Tienes $" + dolares + " dólares")
 
 
-menu = """
-Bienvenido al conversor de monedas 💰
+   menu = """
+   Bienvenido al conversor de monedas 💰
 
-1 - Pesos colombianos
-2 - Pesos argentinos
-3 - Pesos mexicanos
+   1 - Pesos colombianos
+   2 - Pesos argentinos
+   3 - Pesos mexicanos
 
-Elige una opción: """
+   Elige una opción: """
 
-opcion = int(input(menu))
+    opcion = int(input(menu))
 
-if opcion == 1:
-    conversor("colombianos", 3875)
-elif opcion == 2:
-    conversor("argentinos", 65)
-elif opcion == 3:
-    conversor("mexicanos", 24)
-else:
-    print('Ingresa una opción correcta por favor')
+    if opcion == 1:
+        conversor("colombianos", 3875)
+    elif opcion == 2:
+        conversor("argentinos", 65)
+    elif opcion == 3:
+        conversor("mexicanos", 24)
+    else:
+        print('Ingresa una opción correcta por favor')
+
+if __name__ == "__main__":
+    run()
