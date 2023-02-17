a16d92ae153ca2ac256a8eee52fff6d796b358d3
Natalia Garcia <ygaarcia@omd.com.co>
2023-02-17 11:46:48 -0500

    Este es el primer commit de diccionario_picachu

---
+++ Ejercicios/diccionario_picachu.py
@@ -0,0 +1,27 @@
+def run ():
+    picachu = {
+        "vida" = 100,
+        "ataque" = 30,
+    }
+
+    gigglypuff ={
+        "vida" = 100,
+        "ataque" = 20
+    }
+
+        turno = 1
+    while picachu Vida > 0 and gigglypuff Vida > 0:
+        if turno == 1:
+            gigglypuff  Vida = gigglypuff Vida - picachu Ataque
+            turno = 0
+        if turno == 0:
+            picachuVida = picachuVida - gigglypuffAtaque
+            turno = 1
+    print("el gandor fue: ")
+    if picachu Vida > 0:
+        print("picachu")
+    else:
+        print("gigglypuff")
+
+if __name__ == "__main__":
+    run()
