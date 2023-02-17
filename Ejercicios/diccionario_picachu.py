34dceb83d22820c163f0be45795a4ee0362a65bd
Natalia Garcia <ygaarcia@omd.com.co>
2023-02-17 15:59:08 -0500

    Se realizo unos cambios nombres

--- Ejercicios/diccionario_picachu.py
+++ Ejercicios/diccionario_picachu.py
@@ -10,19 +10,19 @@ def run ():
         " turno": 1,
     }
 
-        
-    while picachu Vida > 0 and gigglypuff Vida > 0:
-        if turno == 1:
-            Vida = Vida - Ataque
-            turno = 0
-        if turno == 0:
-            Vida = Vida - Ataque
-            turno = 1
-    print("el gandor fue: ")
-    if picachu > 0:
-        print("picachu")
-    else:
-        print("gigglypuff")
+
+while Vida > 0 and Vida > 0:
+    if turno == 1:
+        gigglypuff Vida = gigglypuff Vida - picachu Ataque
+        turno = 0
+    if turno == 0:
+        picachu Vida = picachu Vida - gigglypuff Ataque
+        turno = 1
+print("el gandor fue: ")
+if picachu Vida > 0:
+    print("picachu")
+else:
+    print("gigglypuff")
 
 if __name__ == "__main__":
     run()
